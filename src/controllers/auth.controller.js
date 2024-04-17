c57e253c69ee8557f2ecc8d93317a2bfdad1cd19
esmanur-uzun <esmabitmez@gmail.com>
2024-04-17 17:10:29 +0300

    worked on error

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,38 +1,30 @@
-const user = require("../models/user.model")
-const bcrypt =  require("bcrypt")
-const APIError = require("../utils/errors")
+const user = require("../models/user.model");
+const bcrypt = require("bcrypt");
+const APIError = require("../utils/errors");
+const Response = require("../utils/response");
 
+const login = async (req, res) => {
+  console.log(req.body);
+  return res.json(req.body);
+};
 
-const login =async(req,res) =>{
-    console.log(req.body)
-    return res.json(req.body)
-}
+const register = async (req, res) => {
+  const { email } = req.body;
+  const userCheck = await user.findOne({ email });
 
-const register =async(req,res) =>{
-    const {email} = req.body
-    const userCheck = await user.findOne({email})
+  if (userCheck) {
+    throw new APIError("Kullanıcı zaten mevcut", 401);
+  }
+  req.body.password = await bcrypt.hash(req.body.password, 10);
+  const userSave = new user(req.body);
+  await userSave
+    .save()
+    .then((register_response) => {
+      return new Response(register_response, "Kayıt başarılı").created(res);
+    })
+    .catch((err) => {
+      throw new APIError("Kayıt Başarısız",400)
+    });
+};
 
-    if(userCheck){
-       throw new APIError("Kullanıcı zaten mevcut",401)
-    }
-    req.body.password = await bcrypt.hash(req.body.password,10)
-    console.log(req.body.password);
-
-    try {
-        const userSave = new user(req.body)
-        await userSave.save().then((register_response)=>{
-            return res.status(201).json({
-                success:true,
-                data:register_response,
-                message:"kayıt başarılı"
-            })
-        }).catch((err)=>{
-            console.log(err);
-        })
-    } catch (error) {
-        console.log(error);
-    }
-    console.log(req.body)
-}
-
-module.exports={login,register}
+module.exports = { login, register };
