4e14258536a79e7079c33757f9652c9fe1673059
esmanur-uzun <esmabitmez@gmail.com>
2024-04-26 12:17:44 +0300

    Cores processing has been done

--- app.js
+++ app.js
@@ -7,10 +7,15 @@ const router = require("./src/routers")
 const port = process.env.PORT || 5001
 const errorHandler = require("./src/middlewares/errorHandler")
 const cors = require("cors")
+const corsOption = require("./src/helpers/corsOption")
+
 
 app.use(express.json())
 app.use(express.json({limit:"50mb"}))
 app.use(express.urlencoded({limit:"50mb",extended:true,parameterLimit:5000}))
+
+// app.use(cors(corsOption))
+
 
 app.use("/api",router)
 
--- src/helpers/corsOption.js
+++ src/helpers/corsOption.js
@@ -1,13 +1,19 @@
-const whiteList = ["http://localhost:3000/"]
+const APIError = require("../utils/errors");
 
-const corsOption = (req,callback) =>{
-    let corsOptions
-    if(whiteList.indexOf(req.header("Origin")) !== -1)
-        corsOptions = {origin:true}
-    else corsOptions = {origin:false}
+const whiteList = ["http://localhost:3003"];
 
-    callback(null,corsOptions)
-    
-}
+const corsOption = (req, callback) => {
+  let corsOptions;
+  console.log(req.header("Origin"));
+  if ( whiteList.indexOf(req.header("Origin")) !== -1) {
+    console.log("if içinde");
+    callback(null,true)
+  } else {
+    console.log("else içinde");
+    callback(new APIError('Not allowed by CORS'))
+  } 
 
-module.exports = corsOption
+  
+};
+
+module.exports = corsOption;
