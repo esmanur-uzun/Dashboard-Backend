1515e5e6f7e7dfd1e2b5d059c06dea1ebd6e925c
esmanur-uzun <esmabitmez@gmail.com>
2024-05-06 22:32:02 +0300

    created product model

---
+++ src/app/product/model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose")
+
+const productSchema = new mongoose.Schema({
+    productName:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    description:{
+        type: String,
+        required: true,
+        trim:true,
+    },
+    price:{
+        type: Number,
+        required:true,
+        trim:true,
+    },
+    stock:{
+        type : Number,
+        required: true,
+        trim: true
+    }
+
+},{collection:"users",timestamps:true})
+
+const product = mongoose.model("product",productSchema)
+module.exports = product
