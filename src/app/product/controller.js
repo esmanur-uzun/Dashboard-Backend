53873f7e361356daaff6eb1a1004349ce9649edb
esmanur-uzun <esmabitmez@gmail.com>
2024-05-10 10:45:27 +0300

    project completed

--- src/app/product/controller.js
+++ src/app/product/controller.js
@@ -1,6 +1,7 @@
 const APIError = require("../../utils/errors");
 const Response = require("../../utils/response");
 const product = require("./model");
+const mongoose = require("mongoose");
 
 const addProduct = async (req, res) => {
   try {
@@ -9,18 +10,60 @@ const addProduct = async (req, res) => {
     return new Response(addProduct_response, "Kayıt başarılı").created(res);
   } catch (error) {
     console.log(error);
-    throw new APIError("Ürün Ekleme İşlemi Başarısız",400)
+    throw new APIError("Ürün Ekleme İşlemi Başarısız", 400);
   }
 };
 
 const getProduct = async (req, res) => {
   try {
     const products = await product.find();
-    return new Response(products, "Ürünler başarıyla alındı").send(res);
+    return new Response(products, "Ürünler başarıyla alındı").success(res);
   } catch (error) {
     console.error(error);
     throw new APIError("Ürünleri Alma İşlemi Başarısız", 500);
   }
 };
 
-module.exports = {addProduct,getProduct}
+const deleteProduct = async (req, res) => {
+  try {
+    // Doğru ObjectId oluşturulması için req.params.id değerini bir dizeye çevirin
+    const productId = new mongoose.Types.ObjectId(req.params.id);
+
+    const deletedProduct = await product.findByIdAndDelete(productId);
+
+    if (!deletedProduct) {
+      return new Response(null, "Ürün bulunamadı").error404(res);
+    }
+
+    return new Response(deletedProduct, "Ürün başarıyla silindi").success(res);
+  } catch (error) {
+    console.error(error);
+    throw new APIError("Ürün Silme İşlemi Başarısız", 500);
+  }
+};
+
+const updateProduct = async (req, res) => {
+  try {
+    const productId = req.params.id;
+    const updatedFields = req.body; // Güncellenecek alanları istek gövdesinden alın
+
+    // Ürünü güncellemek için findByIdAndUpdate() metodunu kullanın
+    const updatedProduct = await product.findByIdAndUpdate(
+      productId,
+      updatedFields,
+      { new: true }
+    );
+
+    if (!updatedProduct) {
+      return new Response(null, "Ürün bulunamadı").error404(res);
+    }
+
+    return new Response(updatedProduct, "Ürün başarıyla güncellendi").success(
+      res
+    );
+  } catch (error) {
+    console.error(error);
+    throw new APIError("Ürün Güncelleme İşlemi Başarısız", 500);
+  }
+};
+module.exports = { addProduct, getProduct, deleteProduct,updateProduct };
--- src/app/product/router.js
+++ src/app/product/router.js
@@ -1,8 +1,9 @@
 const router = require("express").Router()
 const productValidation = require("../../middlewares/validation/product.validation")
-const { addProduct, getProduct } = require("./controller")
+const { addProduct, getProduct, deleteProduct, updateProduct } = require("./controller")
 
 router.post("/product",productValidation.checkProduct,addProduct)
 router.get("/product",getProduct)
-
+router.delete("/product/:id",deleteProduct)
+router.put("/product/:id",updateProduct)
 module.exports = router
